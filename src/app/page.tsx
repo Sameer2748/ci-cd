af83020c4c0bcfa7460c6315f4249772f56674b9
sameer2748 <mrao27488@gmail.com>
2025-02-02 00:57:07 +0530

    added yml file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <h1 className="text-xl font-bold">Happy birthday harkirat</h1>
+      <h1 className="text-xl font-bold">Happy birthday harkirat </h1>
     </div>
   );
 }
